035d440007764820b3ac7c933a394eecad7aec38
Jefferson Ruiz <jefferson.ruiz2108@gmail.com>
2020-07-25 11:01:58 -0500

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tiendas_de_barrio_online.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
