493a97f597b69f39be80104878d827ae9d878c13
Abraham <eas.gavilanez@yavirac,edu,ec>
2020-07-31 05:32:11 -0500

    Pantallas Login/Registro

---
+++ TiendasOnlineWeb/Areas/Identity/Data/TiendasOnlineWebAppUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TiendasOnlineWeb.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the TiendasOnlineWebAppUser class
+    public class TiendasOnlineWebAppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName= "nvarchar(110)")]
+        public string Nombre { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(110)")]
+        public string Apellido { get; set; }
+
+    }
+}
---
+++ TiendasOnlineWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TiendasOnlineWeb.Areas.Identity.Data;
+using TiendasOnlineWeb.Data;
+
+[assembly: HostingStartup(typeof(TiendasOnlineWeb.Areas.Identity.IdentityHostingStartup))]
+namespace TiendasOnlineWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TiendasOnlineWebDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TiendasOnlineWebDbContextConnection")));
+
+                services.AddDefaultIdentity<TiendasOnlineWebAppUser>(options => 
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<TiendasOnlineWebDbContext>();
+            });
+        }
+    }
+}
